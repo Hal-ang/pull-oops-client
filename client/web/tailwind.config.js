e6136ec7dbf3278759e575ef13c17cdd0570dcb1
hal-ang <jpjgv05@gmail.com>
2022-04-17 02:56:54 +0900

    Update tailwind css

--- client/web/tailwind.config.js
+++ client/web/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    // colors: {
+    //   'up-blue': '#4AA5C8',
+    // },
+    // fontFamily: {
+    //   Roboto: '"Roboto"',
+    //   NotoSansKR: '"Noto Sans KR"',
+    // },
     extend: {},
   },
   plugins: [],
