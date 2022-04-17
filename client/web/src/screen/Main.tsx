45f97e0a4fc9f5085def49ed66de7f61f1515ba5
hal-ang <jpjgv05@gmail.com>
2022-04-17 13:59:49 +0900

    Fix conflict

--- client/web/src/screen/Main.tsx
+++ client/web/src/screen/Main.tsx
@@ -24,7 +24,7 @@ const Main = () => {
 
   return (
     <div className="h-screen w-full">
-      <div id="main_header" className="fixed w-full flex">
+      <div id="main_header" className=" w-full flex">
         <div className="w-full relative pt-8 pb-5 bg-white z-10">
           <div className="px-4 justify-between flex items-center z-20">
             {steps.map((_, index) => (
@@ -53,15 +53,45 @@ const Main = () => {
         </div>
         <div
           style={{top: 72}}
-          className="h-8 w-full bg-gradient-to-b from-white to-transparent absolute"></div>
+          className="h-8 w-full bg-gradient-to-b from-white to-transparent absolute z-20"></div>
       </div>
-      <div style={{height: 72}} className="flex-1"></div>
-      <div className="w-full px-6 h-full overflow-y-scroll snap-y pt-10 pb-10">
+      <div className="w-full px-6 h-full overflow-y-scroll snap-y pt-10 pb-36">
         {steps.map((step, index) => (
           <div
             style={{height: cardHeight}}
             key={index}
-            className="w-full bg-black mb-8 rounded-md  snap-center"></div>
+            className="w-full bg-black mb-8  snap-center relative rounded-md">
+            <img
+              src={step.imageUrl}
+              className="object-cover w-full h-full rounded-md"
+            />
+            <div className="w-full h-full absolute top-0 rounded-md bg-[#0A0A0A] opacity-30"></div>
+            <div className="w-full h-full absolute top-0 flex flex-col px-4 pb-4">
+              <div className="grow"></div>
+              <div className="flex justify-between text-white">
+                <div>
+                  <div className="font-Roboto font-bold" style={{fontSize: 40}}>
+                    {step.id + 1}
+                  </div>
+                  <div className="font-NotoSansKR font-bold whitespace-pre text-3xl mt-1">
+                    {step.name}
+                  </div>
+                  <div className="font-NotoSansKR font-bold whitespace-pre text-xl mt-1">
+                    {step.subTitle}
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="grow flex"></div>
+                  <div className="text-right text-3xl">🎉</div>
+                  <div className="font-NotoSansKR font-regular mt-4 text-xs">{`${
+                    step.rep
+                  }rep ${step.rest ? `${step.rest}res` : ''}${
+                    step.set
+                  }set`}</div>
+                </div>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
--- client/web/tailwind.config.js
+++ client/web/tailwind.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    // colors: {
-    //   'up-blue': '#4AA5C8',
-    // },
-    // fontFamily: {
-    //   Roboto: '"Roboto"',
-    //   NotoSansKR: '"Noto Sans KR"',
-    // },
+    fontFamily: {
+      Roboto: '"Roboto"',
+      NotoSansKR: '"Noto Sans KR"',
+    },
     extend: {},
   },
   plugins: [],
